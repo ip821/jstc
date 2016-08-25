0ad3e928ca6144bb5f6dd057ef303471657c972c
igor <igor>
2016-08-25 12:26:04 +0300

    Played with node-ffi

--- boot_electron.js
+++ boot_electron.js
@@ -3,6 +3,8 @@ var electron = require('electron');
 var BrowserWindow = electron.BrowserWindow;
 var app = electron.app;
 const path = require('path');
+const ffi = require("ffi");
+const ref = require("ref");
 
 var mainWindow;
 
@@ -10,18 +12,40 @@ app.setAppPath(__dirname);
 
 var baseDir = "/" + path.basename(__dirname) + "/";
 
-var logToBrowser = function(msg) {
+var logToBrowser = function (msg) {
     mainWindow.webContents.executeJavaScript("console.log('" + msg + "');");
 }
 
 app.on('ready', () => {
+    // var Ucs2String = {
+    //     name: "CString",
+    //     indirection: 1,
+    //     size: ref.sizeof.pointer,
+    //     get: function (buffer, offset) {
+    //         var _buf = buffer.readPointer(offset)
+    //         // TODO: a *real* way to detect the end of the ucs2 string. this is a band-aid...
+    //         return _buf.reinterpret(10000).toString('ucs2')
+    //     },
+    //     set: function (buffer, offset, string) {
+    //         var _buf = new Buffer(Buffer.byteLength(string, 'ucs2') + 1)
+    //         _buf.write(string, 'ucs2')
+    //         _buf[_buf.length - 1] = 0
+    //         return buffer.writePointer(_buf, offset)
+    //     }
+    // }
+
+    // var userLib = ffi.Library("user32.dll", {
+    //     "MessageBoxW": ["int", ["int", Ucs2String, Ucs2String, "uint"]]
+    // });
+    // userLib.MessageBoxW(0, "asd", "фыв", 0);
+
     mainWindow = new BrowserWindow({ width: 1200, height: 800 });
     logToBrowser("Electron version: " + process.versions['electron']);
     logToBrowser("__dirname: " + __dirname);
     logToBrowser("basedir: " + baseDir);
 
     electron.protocol.interceptFileProtocol('file',
-        function(request, callback) {
+        function (request, callback) {
             var url = request.url;
             logToBrowser("Requested url: " + url);
             var pathReal = url.replace("file:///", "");
@@ -41,7 +65,7 @@ app.on('ready', () => {
             logToBrowser("Real url: " + pathReal);
             callback({ path: pathReal });
         },
-        function(error) {
+        function (error) {
             if (error) {
                 console.log(error);
                 return;
---
+++ readme.md
@@ -0,0 +1,4 @@
+Npm install do liek this:
+    npm i --arch=ia32
+Please do after npm i
+    node_modules\.bin\electron-rebuild --arch=ia32
